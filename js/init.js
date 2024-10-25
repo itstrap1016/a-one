e1b087f369fb288b7965ea1f2d333cefa1a32466
itstrap1016 <itstrap1016@gmail.com>
2024-10-25 12:34:22 +0900

    오전 작업

--- js/init.js
+++ js/init.js
@@ -19,9 +19,31 @@ const myFullpage = new fullpage("#fullpage", {
     const activeNavList = document.querySelector(
       `#right_nav .${nowAnchor}_list`
     );
-
+    const header = document.querySelector("#header");
+    const rightNav = document.querySelector("#right_nav");
     beforeNavList.classList.remove("active_now");
     activeNavList.classList.add("active_now");
+    if (beforeAnchor !== "family_site") {
+      const beforeText = document.querySelector(
+        `#fullpage .section.${beforeAnchor} .animation_text`
+      );
+      beforeText.classList.remove("active_now");
+    }
+    if (nowAnchor !== "family_site") {
+      const activeText = document.querySelector(
+        `#fullpage .section.${nowAnchor} .animation_text`
+      );
+      activeText.classList.add("active_now");
+    }
+
+    if (beforeAnchor === "family_site") {
+      header.classList.remove("white_bg");
+      rightNav.classList.remove("white_bg");
+    }
+    if (nowAnchor === "family_site") {
+      header.classList.add("white_bg");
+      rightNav.classList.add("white_bg");
+    }
   },
 });
 
@@ -31,5 +53,21 @@ const rightNavInit = () => {
   const activeNavList = document.querySelector(`#right_nav .${anchor}_list`);
   activeNavList.classList.add("active_now");
 };
+const aniTextInit = () => {
+  const activeSection = myFullpage.getActiveSection();
+  const anchor = activeSection.anchor;
+  if (anchor === "family_site") return;
+  const activeText = document.querySelector(
+    `#fullpage .section.${anchor} .animation_text`
+  );
+  activeText.classList.add("active_now");
+};
+const familySiteInit = () => {
+  const header = document.querySelector("#header");
+  const rightNav = document.querySelector("#right_nav");
+  header.classList.add("white_bg");
+  rightNav.classList.add("white_bg");
+};
 
 rightNavInit();
+aniTextInit();
